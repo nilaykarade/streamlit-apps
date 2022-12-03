23bbdc93084718e1790e66136d8d33e4ba3e0344
nilaykarade <nilaykarade@gmail.com>
2022-12-03 17:22:29 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-[theme]\n\
-primaryColor = '#d33682'\n\
-backgroundColor = '#002b36'\n\
-secondaryBackgroundColor = '#586e75'\n\
-textColor = '#fff'\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
